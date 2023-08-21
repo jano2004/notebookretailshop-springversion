d5dc9f0db20f5e6b0ab2a1a9e8ea8aa9e762e3a0
Jan Wagner <janpaulwagner2004@gmail.com>
2023-08-21 11:17:12 +0200

    style for gaming notebook

---
+++ src/main/java/serverapplication/demo/Computer/Gaming/Notebook/GamingNotebookStyle.java
@@ -0,0 +1,20 @@
+package serverapplication.demo.Computer.Gaming.Notebook;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class GamingNotebookStyle {
+
+    @GetMapping(value = "/css/gamingNB.css", produces = "text/css")
+    public byte[] getStylesheet() throws IOException {
+        Resource resource = new ClassPathResource("static/css/gamingNB.css");
+        try (InputStream inputStream = resource.getInputStream()) {
+            return inputStream.readAllBytes();
+        }
+    }
+}
